eddf88c5c3c6f4a941e9b95a775271c5ecd0bb7a
ContentManager <codeworks@prd.is>
2024-06-19 15:22:37 +0900

    Bump to 1.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,7 +44,7 @@ tasks {
         archiveVersion.set("")
     }
     runServer {
-        minecraftVersion("1.20.6")
+        minecraftVersion("1.21")
         jvmArgs = listOf("-Dcom.mojang.eula.agree=true")
     }
 }
